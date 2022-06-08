dfabcf9cdb5cb012ef71c9ba0617d94f3342c776
Keaton Hutto <keatonhutto8@gmail.com>
2022-06-08 04:41:58 -0500

    updaye

---
+++ update_exercises.sql
@@ -0,0 +1,7 @@
+select name from albums;
+update albums set sales = sales * 10
+select name from albums where release_date < 1980;
+update albums set release_date = release_date = 1800 where release_date < 1980;
+select name from albums where artist = 'Michael Jackson';
+update albums set artist = 'Peter Jackson' where artist = 'Michael Jackson';
+
